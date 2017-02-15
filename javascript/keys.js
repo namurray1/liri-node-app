3402451e9d2a8890d7789e1582d8a828eb2ed95a
Alex Murray <namurray1@gmail.com>
2017-02-15 17:38:21 -0500

    Added API keys for Twitter

---
+++ javascript/keys.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+  consumer_key: 'njdb109tSn3HlU36q5WrX0M59',
+  consumer_secret: 'R48eVnPF3NDWDrhd9OIvO9gydcAS2RsMz63o77yergedT93k69',
+  access_token_key: '6688732-whnJk3cygfisSN09gQjQLKy7FxUgxDVZWNaL4PwHu0',
+  access_token_secret: 'mmRhqqR8B161vONJOm9L21hKqD8OAGe4IFCmmkOWWzAea',
+}
